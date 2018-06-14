42d3489152aef88c09878c7f37404d0c9407d08a
QQAnh <31837158+QQAnh@users.noreply.github.com>
2018-06-14 10:46:02 +0700

    new

--- routes/api.php
+++ routes/api.php
@@ -19,12 +19,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/account', function (Request $request) {
 
     $entries = \App\Account::all();
-    return response()->json($entries, 200);
+    return response()->json($entries, 201);
 });
 
 Route::get('/account/{id}', function (Request $request, $id) {
     $entries = \App\Account::find($id);
-    return response()->json($entries, 200);
+    return response()->json($entries, 201);
 });
 Route::get('/account/destroy/{id}', function ($id) {
     \App\Account::destroy($id);
@@ -33,17 +33,38 @@ Route::get('/account/destroy/{id}', function ($id) {
 });
 Route::get('/user', function (Request $request) {
     $entries = \App\User::all();
-    return response()->json($entries, 200);
+    return response()->json($entries, 201);
 });
 Route::get('/user/{id}', function (Request $request, $id) {
     $entries = \App\User::find($id);
-    return response()->json($entries, 200);
+    return response()->json($entries, 201);
 });
 Route::get('/user/destroy/{id}', function ($id) {
     \App\User::destroy($id);
     return "destroy";
 
 });
+Route::post('/user/hello', function (Request $request) {
+    $entries = [
+        'Fullname' => 'Lê Văn Luyện',
+        'Date_of_Birth' => '11-02-1992',
+        'Email' => 'qưeqew@gmail.com',
+        'Phone' => '132131313',
+        'Address' => 'Hà TĨnh '
+    ];
+    $user = [
+        "Fullname" => $request->input(''),
+        "Date_of_Birth" =>$request->input('Date_of_Birth'),
+        "Email" =>$request->input('Email'),
+        "Phone" =>$request->input('Phone'),
+        "Address" =>$request->input('Address')
+    ];
+    $entries [] = $user;
+    return response()->json($entries, 200);
+
+
+});
+
 
 
 
