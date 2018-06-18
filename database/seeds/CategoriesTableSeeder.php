5d5d09d6104efac4953e0028c0e7d5d1fbf8f960
QQAnh <31837158+QQAnh@users.noreply.github.com>
2018-06-18 23:22:22 +0700

    Update api

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+
 
 class CategoriesTableSeeder extends Seeder
 {
@@ -16,26 +18,22 @@ class CategoriesTableSeeder extends Seeder
                 'id'=>'1',
                 'title'=>'Smart Phone',
                 'image_url'=>'http://www.androidguys.com/wp-content/uploads/2017/08/Geekbuying-sale.png	',
-                'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
-                'update_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+
             ],[
                 'id'=>'2',
                 'title'=>'LAPTOP',
                 'image_url'=>'https://www.earticleblog.com/wp-content/uploads/2017/10/Best-offers-on-Laptop.jpg	',
-                'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
-                'update_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+
             ],[
                 'id'=>'3',
                 'title'=>'PC',
                 'image_url'=>'https://zoutons.com/assets/images/originals/blog/Untitled-1_1504272715.jpg	',
-                'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
-                'update_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+
             ],[
                 'id'=>'4',
                 'title'=>'Game Consoles & Accessories',
                 'image_url'=>'https://slickdeals.net/blog/wp-content/uploads/2017/11/best-black-friday-videogames-hero-revised.png	',
-                'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
-                'update_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+
             ],
 
         ]);
