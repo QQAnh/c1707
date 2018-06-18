e9436768f0fe0f67de5131ac498c1d3cad60cd8f
QQAnh <31837158+QQAnh@users.noreply.github.com>
2018-06-18 23:26:11 +0700

    Update api

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -15,22 +15,22 @@ class CategoriesTableSeeder extends Seeder
     {
         DB::table('products')->insert([
             [
-                'id'=>'1',
+                'id'=>1,
                 'title'=>'Smart Phone',
                 'image_url'=>'http://www.androidguys.com/wp-content/uploads/2017/08/Geekbuying-sale.png	',
 
             ],[
-                'id'=>'2',
+                'id'=>2,
                 'title'=>'LAPTOP',
                 'image_url'=>'https://www.earticleblog.com/wp-content/uploads/2017/10/Best-offers-on-Laptop.jpg	',
 
             ],[
-                'id'=>'3',
+                'id'=>3,
                 'title'=>'PC',
                 'image_url'=>'https://zoutons.com/assets/images/originals/blog/Untitled-1_1504272715.jpg	',
 
             ],[
-                'id'=>'4',
+                'id'=>4,
                 'title'=>'Game Consoles & Accessories',
                 'image_url'=>'https://slickdeals.net/blog/wp-content/uploads/2017/11/best-black-friday-videogames-hero-revised.png	',
 
