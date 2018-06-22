3a01ca6bc022168ff8c89ba55916ac2bfae53816
QQAnh <31837158+QQAnh@users.noreply.github.com>
2018-06-22 09:25:48 +0700

    Update api

--- app/Category.php
+++ app/Category.php
@@ -6,7 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    public function product() {
-        return $this->hasMany('Product', 'id');
-    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -62,12 +62,7 @@ Route::get('/user/destroy/{id}', function ($id) {
 
 Route::resource('products', 'Endpoint\ProductsController');
 Route::resource('category', 'Endpoint\CategoriesController');
-Route::get('/products/hello', function (Request $request, $id) {
-    $categoryId = 10;
-    $category = Mage::getModel('catalog/category')->load($categoryId);
-    return response()->json($category, 201);
 
-});
 
 
 
