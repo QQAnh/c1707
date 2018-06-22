c1316b615c6d5f72217f4a913d8e7fc6af2e038c
QQAnh <31837158+QQAnh@users.noreply.github.com>
2018-06-22 10:14:52 +0700

    Update api

--- routes/api.php
+++ routes/api.php
@@ -62,7 +62,7 @@ Route::get('/user/destroy/{id}', function ($id) {
 
 Route::resource('products', 'Endpoint\ProductsController');
 Route::resource('category', 'Endpoint\CategoriesController');
-Route::get('/products/smartphone', function () {
+Route::get('smartphone', function () {
     $product = \App\Product::where('category',1)->get();
     return response()->json($product, 201);
 });
